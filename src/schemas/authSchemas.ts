9bae661ceac9f5c9b699fca8d1842268ca56819a
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2023-01-24 19:19:30 -0300

    fix: mudança schema para não aceitar somente email

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -2,12 +2,12 @@ import Joi from 'joi';
 import { IAccountRegisterData, IAccountLoginData } from '../types/accountTypes';
 
 export const registerSchema = Joi.object<IAccountRegisterData>({
-    user: Joi.string().email().required(),
+    user: Joi.string().required(),
     password: Joi.string().min(8).required(),
     confirmPassword: Joi.valid(Joi.ref('password'))
 })
 
 export const loginSchema = Joi.object<IAccountLoginData>({
-    user: Joi.string().email().required(),
+    user: Joi.string().required(),
     password: Joi.string().min(8).required(),
 })
